67c168d12ff24f1fdc85f81995b70d7dca4d0bfc
Robert Nagy <ronagy@icloud.com>
2024-06-13 10:15:08 +0300

    add tests

---
+++ test/parse-headers.js
@@ -0,0 +1,17 @@
+import { test } from 'tap'
+import { parseHeaders } from '../lib/utils.js'
+
+test('parse rawheaders', (t) => {
+  const headers = parseHeaders([
+    Buffer.from('Content-Type'),
+    Buffer.from('application/json'),
+    Buffer.from('Content-Length'),
+    Buffer.from('10'),
+  ])
+  t.strictSame(headers, {
+    'content-type': 'application/json',
+    'content-length': '10',
+  })
+
+  t.end()
+})

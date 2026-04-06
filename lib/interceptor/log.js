nxtedition/nxt-undici

commit 2d5adef76a252b74c39e194be7f2ee43a5591c73

feat: track active requests globally in log interceptor
Expose in-flight log interceptor handlers via globalThis.__undici_requests for debugging and introspection.

Co-Authored-By: Claude Sonnet 4.6 <noreply@anthropic.com>

lib/interceptor/log.js
@@ -19,6 +19,9 @@ class Handler extends DecoratorHandler {
   #statusCode
   #headers
 
+  #globalIndex = -1
+  #globalArray = (globalThis.__undici_requests ??= [])
+
   constructor(logOpts, opts, { handler }) {
     super(handler)
 
@@ -31,6 +34,8 @@ class Handler extends DecoratorHandler {
 
     this.#logger.debug('upstream request started')
     this.#timing.created = this.#created + performance.timeOrigin
+
+    this.#globalIndex = this.#globalArray.push(this) - 1
   }
 
   onConnect(abort) {
@@ -110,6 +115,8 @@ class Handler extends DecoratorHandler {
       this.#logger.debug(data, 'upstream request completed')
     }
 
+    this.onDone()
+
     super.onComplete()
   }
 
@@ -137,7 +144,20 @@ class Handler extends DecoratorHandler {
       this.#logger.error(data, 'upstream request failed')
     }
 
+    this.onDone()
+
     super.onError(err)
+  }
+
+  onDone() {
+    if (this.#globalIndex !== -1) {
+      const tmp = this.#globalArray.pop()
+      if (tmp !== this) {
+        this.#globalArray[this.#globalIndex] = tmp
+        tmp.#globalIndex = this.#globalIndex
+      }
+      this.#globalIndex = -1
+    }
   }
 }
 

commit be1f9aea7a5116cb0345f59afb531d0144b8cfa3

fix: use WeakMap in onDone() to avoid cross-class crash introduced in 2d5adef

lib/interceptor/log.js
@@ -1,5 +1,8 @@
 import { DecoratorHandler } from '../utils.js'
 
+const globalArray = (globalThis.__undici_requests ??= [])
+const globalIndexMap = new WeakMap()
+
 class Handler extends DecoratorHandler {
   #opts
   #logger
@@ -19,9 +22,6 @@ class Handler extends DecoratorHandler {
   #statusCode
   #headers
 
-  #globalIndex = -1
-  #globalArray = (globalThis.__undici_requests ??= [])
-
   constructor(logOpts, opts, { handler }) {
     super(handler)
 
@@ -35,7 +35,7 @@ class Handler extends DecoratorHandler {
     this.#logger.debug('upstream request started')
     this.#timing.created = this.#created + performance.timeOrigin
 
-    this.#globalIndex = this.#globalArray.push(this) - 1
+    globalIndexMap.set(this, globalArray.push(this) - 1)
   }
 
   onConnect(abort) {
@@ -151,13 +151,14 @@ class Handler extends DecoratorHandler {
   }
 
   onDone() {
-    if (this.#globalIndex !== -1) {
-      const tmp = this.#globalArray.pop()
+    const idx = globalIndexMap.get(this)
+    if (idx !== undefined) {
+      globalIndexMap.delete(this)
+      const tmp = globalArray.pop()
       if (tmp !== this) {
-        this.#globalArray[this.#globalIndex] = tmp
-        tmp.#globalIndex = this.#globalIndex
+        globalArray[idx] = tmp
+        globalIndexMap.set(tmp, idx)
       }
-      this.#globalIndex = -1
     }
   }
 }
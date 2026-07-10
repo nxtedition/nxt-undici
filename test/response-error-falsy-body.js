d0d90318dbb5254d47d6109d1c5036bc89f16c65
Robert Nagy <ronagy@icloud.com>
2026-07-10 21:09:37 +0200

    fix(response-error): retain falsy JSON bodies

---
+++ test/response-error-falsy-body.js
@@ -0,0 +1,35 @@
+import { test } from 'tap'
+import responseError from '../lib/interceptor/response-error.js'
+
+function captureError(payload) {
+  let error
+  const dispatch = responseError()((_opts, handler) => {
+    handler.onConnect(() => {})
+    handler.onHeaders(400, { 'content-type': 'application/json' }, () => {})
+    handler.onData(Buffer.from(payload))
+    handler.onComplete({})
+  })
+
+  dispatch(
+    { error: true, origin: 'http://example.test', path: '/', method: 'GET', headers: {} },
+    {
+      onError(err) {
+        error = err
+      },
+    },
+  )
+  return error
+}
+
+test('response errors retain valid falsy JSON bodies', (t) => {
+  for (const [payload, expected] of [
+    ['false', false],
+    ['0', 0],
+    ['""', ''],
+  ]) {
+    const error = captureError(payload)
+    t.equal(Object.hasOwn(error, 'body'), true, `${payload} is attached`)
+    t.equal(error.body, expected)
+  }
+  t.end()
+})
